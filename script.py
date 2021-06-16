3d522c3a89b42b00fc491b94e8252c7578fe4cf8
jlmiller0818 <79217704+jlmiller0818@users.noreply.github.com>
2021-06-16 11:55:02 -0500

    Sudan vaccination

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import requests
 import json
 import pandas as pd
-fileNames = ["SUD_vaccine_acpt.csv"]
-urls = ["https://covidmap.umd.edu/api/resources?indicator=vaccine_acpt&type=daily&country=Sudan&daterange=20200301-20210609"]
+fileNames = ["SUD_covid_vaccine.csv"]
+urls = ["https://covidmap.umd.edu/api/resources?indicator=covid_vaccine&type=daily&country=Sudan&daterange=20200301-20210609"]
 if(len(fileNames) != len(urls)):
     print("Need the same number of files and urls")
     exit()
